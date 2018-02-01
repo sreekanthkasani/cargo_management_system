3be0ca3c120483f851309dd7836d0e124425fb8f
sreekanthkasani <sreekanthkasani22@gmail.com>
2018-02-01 18:51:33 +0530

    universal footer

---
+++ php/footer.php
@@ -0,0 +1,15 @@
+<div class="footer">
+  <div class="FBG">
+    <div class="footer_resize">
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="FBG">
+    <div class="footer_resize">
+      <p class="leftt">© Copyright Your Site Name.  Dot Com. All Rights Reserved <br />
+        <a href="#">Home</a> | <a href="#">Contact</a> | <a href="#">RSS</a> </p>
+      <p class="rightt"><a href="http://dreamtemplate.com/"> Design by DreamTemplate </a></p>
+      <div class="clr"></div>
+    </div>
+  </div>
+</div>
