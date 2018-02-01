1c0353a23739085e640d95afa5c89c4d2b0600f0
sreekanthkasani <sreekanthkasani22@gmail.com>
2018-02-01 18:43:47 +0530

    projects offered page

---
+++ php/project.php
@@ -0,0 +1,67 @@
+<?php 
+include("header.php");
+?>
+  <div class="slider_bg">
+    <div class="slider_t">
+      <div class="slider_b small">
+        <div class="title_text">
+          <p>Your slogan goes here</p>
+          <h2>Projects </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body">
+    <div class="left">
+      <div class="box no_margin">
+        <div class="box_t">
+          <div class="box_b">
+            <h2>Sub Navigation</h2>
+            <ul>
+              <li><a href="#" class="active">Sub Navigation 1</a></li>
+              <li><a href="#"> Sub Navigation 2</a></li>
+              <li><a href="#"> Sub Navigation 3</a></li>
+              <li><a href="#"> Sub Navigation 4</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="clr"></div>
+      <div class="testi">
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. <br />
+          Cum sociis natoque&nbsp;</p>
+        <p align="right">— John Doe, President, <strong>Company</strong></p>
+      </div>
+    </div>
+    <div class="big_center no_margin">
+      <h2>Our Portfolio</h2>
+      <p class="spec">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget bibendum tellus. Nunc vel imperdiet tellus. Mauris ornare aliquam urna, accumsan bibendum eros auctor ac.</p>
+      <h3>Business Style</h3>
+      <img src="images/portfolio_1.jpg" alt="picture" width="255" height="234" class="img" />
+      <p class="spec">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><a href="#"><img src="images/but_lunch_site.gif" alt="picture" width="91" height="23" border="0" /></a></p>
+      <h3>Clean Portfolio</h3>
+      <img src="images/portfolio_2.jpg" alt="picture" width="255" height="234" class="img" />
+      <p class="spec">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><a href="#"><img src="images/but_lunch_site.gif" alt="picture" width="91" height="23" border="0" /></a></p>
+      <h3>Business &amp; Design</h3>
+      <img src="images/portfolio_3.jpg" alt="picture" width="255" height="234" class="img" />
+      <p class="spec">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><a href="#"><img src="images/but_lunch_site.gif" alt="picture" width="91" height="23" border="0" /></a></p>
+      <div class="lister">
+        <ul>
+          <li><a href="#" class="active">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
