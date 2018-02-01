77e400c4af3e976e0f8e73c0348f7cf4731ef7a9
sreekanthkasani <sreekanthkasani22@gmail.com>
2018-02-01 18:38:45 +0530

    prototype-page

---
+++ php/index1.php
@@ -0,0 +1,86 @@
+<?php
+include("header.php");
+?>
+  <div class="slider_bg">
+    <div class="slider_t">
+      <div class="slider_b">
+        <div id="slider">
+          <ul>
+            <li>
+              <p class="img"><a href="#"><img src="images/screen_1.jpg" alt="screen 1" width="405" height="226" /></a></p>
+              <div class="button"><a href="#"><img src="images/but_launch_project.gif" alt="picture" width="139" height="28" border="0" /></a><span class="right_but"><img src="images/l_arrow.gif" alt="picture" width="84" height="28" border="0" /><img src="images/r_arrow.gif" alt="picture" width="84" height="28" border="0" /></span></div>
+              <h2>Designlab</h2>
+              <p>Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </li>
+            <li>
+              <p class="img"><img src="images/screen_2.jpg" alt="screen 2" width="405" height="226" /></p>
+              <div class="button"><a href="#"><img src="images/but_launch_project.gif" alt="picture" width="139" height="28" border="0" /></a><span class="right_but"><a href="#"><img src="images/l_arrow.gif" alt="picture" width="84" height="28" border="0" /></a><a href="#"><img src="images/r_arrow.gif" alt="picture" width="84" height="28" border="0" /></a></span></div>
+              <h2>Universal Business</h2>
+              <p>Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </li>
+            <li>
+              <p class="img"><img src="images/screen_3.jpg" alt="screen 3" width="405" height="226" /></p>
+              <div class="button"><a href="#"><img src="images/but_launch_project.gif" alt="picture" width="139" height="28" border="0" /></a><span class="right_but"><a href="#"><img src="images/l_arrow.gif" alt="picture" width="84" height="28" border="0" /></a><a href="#"><img src="images/r_arrow.gif" alt="picture" width="84" height="28" border="0" /></a></span></div>
+              <h2>Business Style</h2>
+              <p>Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="box_block">
+    <div class="box">
+      <div class="box_t">
+        <div class="box_b">
+          <h2>Web Development</h2>
+          <img src="images/img_1.gif" alt="picture" width="50" height="50" />
+          <p><strong>Fusce vehicula dignissim ligula. </strong><br />
+            Vestibulum sit amet neque eu neque suscipit consequat quis vel risus. <a href="#">Vestibulum vehicula purus nec dui </a>accumsan fermentum. </p>
+          <p><a href="#" class="more">Read More</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="box_t">
+        <div class="box_b">
+          <h2>Our Services</h2>
+          <img src="images/img_2.gif" alt="picture" width="50" height="50" />
+          <p><strong>Fusce vehicula dignissim ligula. </strong><br />
+            Vestibulum sit amet neque eu neque suscipit consequat quis vel risus. Vestibulum vehicula purus nec dui accumsan fermentum. </p>
+          <p><a href="#" class="more">Read More</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="box no_margin">
+      <div class="box_t">
+        <div class="box_b">
+          <h2>What We Do?</h2>
+          <img src="images/img_3.gif" alt="picture" width="50" height="50" />
+          <p><strong>Fusce vehicula dignissim ligula. </strong><br />
+            Vestibulum sit amet neque eu neque suscipit consequat quis vel risus. Vestibulum vehicula purus nec dui accumsan fermentum. </p>
+          <p><a href="#" class="more">Read More</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="clr"></div>
+  </div>
+  <div class="body">
+    <div class="big_centera">
+      <h2>Welcome to business theme</h2>
+      <p class="spec">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget bibendum tellus. Nunc vel imperdiet tellus. Mauris ornare aliquam urna, accumsan bibendum eros auctor ac.</p>
+      <p>Suspendisse adipiscing rhoncus massa, sit amet sollicitudin quam vulputate non. In non turpis nisl. Curabitur purus mi, pharetra vitae viverra et, mattis sit amet nunc. Quisque enim ipsum, convallis sit amet molestie in, placerat vel urna. Praesent congue auctor elit, nec pretium ipsum volutpat vitae. Vivamus eget ipsum sit amet ipsum tincidunt fermentum.</p>
+      <p>Sed hendrerit neque ac erat condimentum vulputate. Nulla velit massa, dictum et interdum quis, tempus at velit. Nunc gravida luctus mauris at rhoncus. Nulla facilisi. Phasellus iaculis varius nibh a adipiscing. Vestibulum nec laoreet tellus. Integer aliquet lacinia justo vel congue.</p>
+    </div>
+     <div class="big_centerb">
+      <h2>Welcome to business theme</h2>
+      <p class="spec">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget bibendum tellus. Nunc vel imperdiet tellus. Mauris ornare aliquam urna, accumsan bibendum eros auctor ac.</p>
+      <p>Suspendisse adipiscing rhoncus massa, sit amet sollicitudin quam vulputate non. In non turpis nisl. Curabitur purus mi, pharetra vitae viverra et, mattis sit amet nunc. Quisque enim ipsum, convallis sit amet molestie in, placerat vel urna. Praesent congue auctor elit, nec pretium ipsum volutpat vitae. Vivamus eget ipsum sit amet ipsum tincidunt fermentum.</p>
+      <p>Sed hendrerit neque ac erat condimentum vulputate. Nulla velit massa, dictum et interdum quis, tempus at velit. Nunc gravida luctus mauris at rhoncus. Nulla facilisi. Phasellus iaculis varius nibh a adipiscing. Vestibulum nec laoreet tellus. Integer aliquet lacinia justo vel congue.</p>
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
