2358675ea9059f499a87f538a347bb050ea9507c
sreekanthkasani <sreekanthkasani22@gmail.com>
2018-02-01 18:43:13 +0530

    details of the services provided

---
+++ php/services.php
@@ -0,0 +1,58 @@
+<?php
+include("header.php");
+?>
+  <div class="slider_bg">
+    <div class="slider_t">
+      <div class="slider_b small">
+        <div class="title_text">
+          <p>Your slogan goes here</p>
+          <h2>Services </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body">
+     <div class="left">
+      <div class="box no_margin">
+        <div class="box_t">
+          <div class="box_b">
+            <h2>Sub Navigation</h2>
+            <ul>
+              <li><a href="#" class="active">Overview</a></li>
+              <li><a href="#">What we offer</a></li>
+              <li><a href="#">Products List</a></li>
+              <li><a href="#">Project Management</a></li>
+              <li><a href="#">Affiliation</a></li>
+              <li><a href="#">Development</a></li>
+              <li><a href="#">Designing Services</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="big_center no_margin">
+      <h2>Our Service</h2>
+      <p class="spec">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget bibendum tellus. Nunc vel imperdiet tellus. Mauris ornare aliquam urna, accumsan bibendum eros auctor ac.</p>
+      <p>Suspendisse adipiscing rhoncus massa, sit amet sollicitudin quam vulputate non. In non turpis nisl. Curabitur purus mi, pharetra vitae viverra et, mattis sit amet nunc. </p>
+      <h3>Featured Services 1</h3>
+      <img src="images/service_1.png" alt="picture" width="60" height="60" class="img" />
+      <p class="spec">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><a href="#"><img src="images/but_order_now.gif" alt="picture" width="91" height="23" border="0" /></a></p>
+      <h3>Featured Services 2</h3>
+      <img src="images/service_2.png" alt="picture" width="60" height="60" class="img" />
+      <p class="spec">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><a href="#"><img src="images/but_order_now.gif" alt="picture" width="91" height="23" border="0" /></a></p>
+      <h3>Featured Services 3</h3>
+      <img src="images/service_3.png" alt="picture" width="60" height="60" class="img" />
+      <p class="spec">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p><a href="#"><img src="images/but_order_now.gif" alt="picture" width="91" height="23" border="0" /></a></p>
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
